cec28a61126a7ee83d50264d5b87dcb517648612
s1989sandy <sandeepkumar1989@outlook.com>
2018-01-01 19:40:47 +0530

    Add files via upload

---
+++ src/main/java/com/javahome/controller/JewelleryShopController.java
@@ -0,0 +1,33 @@
+package com.javahome.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javahome.model.Product;
+import com.javahome.service.JewelleryShopService;
+
+@RestController
+public class JewelleryShopController {
+
+	private final JewelleryShopService jewelleryShopService;
+
+	@Autowired
+	public JewelleryShopController(JewelleryShopService jewelleryShopService) {
+		this.jewelleryShopService = jewelleryShopService;
+	}
+
+	@RequestMapping(value = "/getDiscount", method = RequestMethod.GET, produces = "application/json")
+	public Product getQuestions(@RequestParam(required = true) String type) {
+
+		Product product = new Product();
+		product.setType(type);
+
+		jewelleryShopService.getProductDiscount(product);
+
+		return product;
+	}
+
+}
